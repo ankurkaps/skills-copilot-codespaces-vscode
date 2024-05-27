cbacf790be8a6c948753d2b1980cb617b2d350f7
Ankur Kapila <17313378+ankurkaps@users.noreply.github.com>
2024-05-27 12:51:17 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,31 +1,61 @@
 // Create a web server
-
-// Create a GET route /comments
-// Create a POST route /comments
-// Create a DELETE route /comments
-// Create a PUT route /comments
-
 const express = require('express');
 const app = express();
+const port = 3000;
 
+// Use the express.static middleware to serve static files
+app.use(express.static('public'));
+
+// Use the express.urlencoded middleware to parse the body of a POST request
+app.use(express.urlencoded({extended: true}));
+
+// Use the express.json middleware to parse the body of a POST request
 app.use(express.json());
 
+// Use the express.static middleware to serve static files
+app.use(express.static('public'));
+
+// Set the application to listen on port 3000
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+// Import the comments module
+const comments = require('./comments');
+
+// Create a GET request to /comments
 app.get('/comments', (req, res) => {
-    res.send('GET comments');
+    res.json(comments);
 });
 
+// Create a POST request to /comments
 app.post('/comments', (req, res) => {
-    res.send('POST comments');
+    const comment = req.body;
+    comments.push(comment);
+    res.json(comment);
 });
 
-app.delete('/comments', (req, res) => {
-    res.send('DELETE comments');
+// Create a DELETE request to /comments
+app.delete('/comments/:id', (req, res) => {
+    const id = req.params.id;
+    comments.splice(id, 1);
+    res.json(comments);
 });
 
-app.put('/comments', (req, res) => {
-    res.send('PUT comments');
+// Create a PUT request to /comments
+app.put('/comments/:id', (req, res) => {
+    const id = req.params.id;
+    comments[id] = req.body;
+    res.json(comments);
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+// Create a GET request to /comments/:id
+app.get('/comments/:id', (req, res) => {
+    const id = req.params.id;
+    res.json(comments[id]);
+});
+
+// Create a GET request to /comments/length
+app.get('/comments/length', (req, res) => {
+    res.json(comments.length);
 });
